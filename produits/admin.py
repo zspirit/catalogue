7c7a1b07cbe734cffb2e27f0f4d6bbc2a11a8d0f
zspirit <zgafaoui@gmail.com>
2016-05-11 01:31:06 +0200

    New commit

--- produits/admin.py
+++ produits/admin.py
@@ -3,7 +3,15 @@ from django.contrib import admin
 # Register your models here.
 from .models import Produit, Tag, Categorie, Image
 
-admin.site.register(Produit)
+class ImageInline(admin.StackedInline):
+    model = Image
+
+class ProduitAdmin(admin.ModelAdmin):
+    inlines = [
+        ImageInline,
+    ]
+    #raw_id_fields = ("Tags","Categories")
+
+admin.site.register(Produit, ProduitAdmin)
 admin.site.register(Tag)
 admin.site.register(Categorie)
-admin.site.register(Image)
--- produits/models.py
+++ produits/models.py
@@ -1,6 +1,7 @@
 from __future__ import unicode_literals
 from django.template.defaultfilters import slugify
 from django.db import models
+from django.conf import settings
 
 # Create your models here.
 class Produit(models.Model):
@@ -12,10 +13,15 @@ class Produit(models.Model):
     tags = models.ManyToManyField('Tag')
     categories = models.ManyToManyField('Categorie')
     visible = models.BooleanField(default=True)
+
     def __unicode__(self):
         return self.title
+
     class Meta:
         ordering = ('title',)
+
+    def get_feature_image(self):
+        return Image.objects.filter(post=self)[:1].get()
 
 class Tag(models.Model):
     """docstring for Tags"""
@@ -34,11 +40,12 @@ class Categorie(models.Model):
 def get_image_filename(instance, filename):
     title = instance.post.title
     slug = slugify(title)
-    return "produits/static/uploads/%s-%s" % (slug, filename) 
+    return "%s-%s" % (slug, filename) 
 
 class Image(models.Model):
-    post = models.ForeignKey(Produit, default=None)
-    image = models.ImageField(upload_to=get_image_filename,
+    post = models.ForeignKey(Produit, default=None, on_delete=models.CASCADE)
+    image = models.ImageField(upload_to=settings.BASE_DIR+settings.STATIC_URL+"produits/static/uploads/%Y/%m/%d/",
                               verbose_name='Image')
     def __unicode__(self):
-        return "Image du produit : %s" % (self.post.title) 
+        return "Image du produit : %s" % (self.post.title)
+
